46f86f710feb7b1286120e553c174392d5a6c2ed
Jason Tham <jason.tham@tempus.com>
2017-10-14 17:51:42 -0500

    updated urls to server client on / and api on /api

--- reactpy/reactpy/urls.py
+++ reactpy/reactpy/urls.py
@@ -24,7 +24,7 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^home/$', views.RootView.as_view()),
+    url(r'^$', views.RootView.as_view()),
 ]
